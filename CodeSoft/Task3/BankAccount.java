2ef5fbe01ac25da6ff36962e8432b14bc29c2aa9
sayalip13 <sayalipatil4162@gmail.com>
2025-03-14 21:04:52 +0530

    m

---
+++ CodeSoft/Task3/BankAccount.java
@@ -0,0 +1,34 @@
+package com.CodeSoft.Task3;
+
+
+//ATM INTERFACE
+
+public class BankAccount {
+
+    // The account balance
+    private int balance;
+
+    // Constructor
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    // Withdraws cash from the account
+    public void withdraw(int amount){
+        if (balance < amount) {
+           // System.out.println("Insufficient funds");
+        }
+
+        balance -= amount;
+    }
+
+    // Deposits cash into the account
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    // Checks the account balance
+    public int getBalance() {
+        return balance;
+    }
+}
